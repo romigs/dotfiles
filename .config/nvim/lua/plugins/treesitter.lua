f80b7054f8929008b62c5cc5fb126f3744c937ea
romigs <121357821+romigs@users.noreply.github.com>
2023-01-27 12:07:57 +0700

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,22 @@
-local status_safe, treesitter = pcall(require, "nvim-treesitter.config")
-if not status_safe then
+local is_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not is_ok then
   return
-treesitter.setup {
+end
+
+
+treesitter.setup ({
   -- A list of parser names, or "all"
   ensure_installed = { "c","cpp", "lua", "rust", "html", "javascript", "php", "json",
   	"scss", "css", "go"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
   highlight = {
     enable = true,
-
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -23,6 +24,9 @@ treesitter.setup {
             return true
         end
     end,
-
+	
   },
-}
+incremental_selection = { enable = true },
+    textobjects = { enable = true },
+})
+
